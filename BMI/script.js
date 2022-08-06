ce9e87caf69268af3c72b731ed9c9cc2db547e91
unknown <you@example.com>
2022-08-06 14:07:22 +0400

    added files

---
+++ BMI/script.js
@@ -0,0 +1,36 @@
+let confirmation = prompt("Welcome, do you want to calculate your BMR, let's GO?");
+console.log(confirmation);
+
+    let Name  = prompt("Please input your Name:")
+    console.log(Name);
+
+    let Weight  = prompt("Please input your Weight with Kilogram:")
+    console.log(Weight);
+
+    let Height = prompt("Please input your Height with Meters:")
+    console.log(Height);
+
+    let Calculate=  Weight/ Height ** 2;
+    console.log("Your BMR is-" + Calculate);
+
+
+        if(Calculate<=18.5) 
+        {
+        console.log("Dear "+Name+ ",Your Body Mass Index is-UnderWeight!") 
+        }
+        
+        else if (Calculate>18.5 && Calculate<=24.9) {
+            console.log("Dear "+Name+ ",Your Body Mass Index is-Normal!") }
+        
+        else if (Calculate>=25 && Calculate<=29.9) {
+            console.log("Dear "+Name+ ",Your Body Mass Index is-OverWeight!") }
+        
+        else if (Calculate>=30 && Calculate<=34.9) {
+            console.log("Dear "+Name+ ",Your Body Mass Index is-Obese!") }
+        
+            else {
+            console.log("Dear "+Name+ ",Your Body Mass Index is-Extremly Obese!") }
+    
+    
+    
+    
